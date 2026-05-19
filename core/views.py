1f79ef3c8a4dd6c42e39d87b79270a05d107c9a1
RaizaArce <raiza.nataly.arce@gmail.com>
2026-05-19 11:15:31 -0500

    Modificación de login.html, login.css, base.html, dashboard.html y views.py(solo importar y dashboard_view )

--- core/views.py
+++ core/views.py
@@ -3,6 +3,12 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 
+#Raiza modificó dashboard_view y añadio los imports de mesa, comanda e insumo
+from mesas.models import Mesa
+from pedidos.models import Comanda
+from inventario.models import Insumo
+from django.db import models
+
 
 def login_view(request):
     if request.method == 'POST':
@@ -27,9 +33,21 @@ def login_view(request):
     return render(request, 'auth/login.html')
 
 
+#segunda modificación by raiza :
 @login_required
 def dashboard_view(request):
-    return render(request, 'core/dashboard.html')
+    mesas_libres = Mesa.objects.filter(estado='LIBRE').count()
+    mesas_ocupadas = Mesa.objects.filter(estado='OCUPADA').count()
+    comandas_activas = Comanda.objects.filter(estado__in=['ABIERTA', 'EN_PREPARACION']).count()
+    alertas_stock = Insumo.objects.filter(stock_actual__lt=models.F('stock_minimo')).count()
+
+    context = {
+        'mesas_libres': mesas_libres,
+        'mesas_ocupadas': mesas_ocupadas,
+        'comandas_activas': comandas_activas,
+        'alertas_stock': alertas_stock,
+    }
+    return render(request, 'core/dashboard.html', context)
 
 
 def logout_view(request):
