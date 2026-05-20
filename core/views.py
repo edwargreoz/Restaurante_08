ba50a8620ae1fa11a5e2881269e08c785a54c32a
RaizaArce <raiza.nataly.arce@gmail.com>
2026-05-20 00:53:12 -0500

    Se siguió implementadno Dashboard y Plano de Mesas - Frontend funcional con diseño, se edito views de core, el dashboard_view para agregar lo de las mesas y los import de fecha, claculo e ingresos, el style.css para dar diseño a las pantallas de dashbaord y mesas y los html del dashbaord y mesas

--- core/views.py
+++ core/views.py
@@ -8,6 +8,10 @@ from mesas.models import Mesa
 from pedidos.models import Comanda
 from inventario.models import Insumo
 from django.db import models
+#Raiza
+from django.utils import timezone #para mostrar fecha y hora en el dashboard
+from datetime import timedelta #para calcular pedidos del dia
+from caja.models import Caja,Pago #para mostrar ingresos del dia en el dashboard
 
 
 def login_view(request):
@@ -40,12 +44,36 @@ def dashboard_view(request):
     mesas_ocupadas = Mesa.objects.filter(estado='OCUPADA').count()
     comandas_activas = Comanda.objects.filter(estado__in=['ABIERTA', 'EN_PREPARACION']).count()
     alertas_stock = Insumo.objects.filter(stock_actual__lt=models.F('stock_minimo')).count()
+    
+    hoy=timezone.now().date()
+    manana=hoy+timedelta(days=1)
+    ventas_hoy=Pago.objects.filter(
+        fecha__date__gte=hoy,
+        fecha__date__lt=manana
+    ).aggregate(total=models.Sum('monto'))['total'] or 0 #si no hay ventas, total sera None, por eso usamos 'or 0' para mostrar 0 en vez de None
+
+    #
+    caja_actual=Caja.objects.filter(estado='ABIERTA').last()
+
+    ultimas_comandas=Comanda.objects.filter(
+        estado__in=['ABIERTA', 'EN_PREPARACION']
+    ).select_related('mesa', 'mozo').order_by('-fecha_apertura')[:5] #mostrar las 5 comandas mas recientes
+
+    alertas_detalle=Insumo.objects.filter(
+        stock_actual__lt=models.F('stock_minimo')
+    )[:5] #mostrar solo 5 alertas de stock
 
     context = {
         'mesas_libres': mesas_libres,
         'mesas_ocupadas': mesas_ocupadas,
         'comandas_activas': comandas_activas,
         'alertas_stock': alertas_stock,
+        
+        'ventas_hoy': ventas_hoy,
+        'caja_actual': caja_actual,
+        'ultimas_comandas': ultimas_comandas,
+        'alertas_detalle': alertas_detalle,
+        
     }
     return render(request, 'core/dashboard.html', context)
 
