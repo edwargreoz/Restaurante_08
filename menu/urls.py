29e6836df4bef03cf9db271f6d1704ead35855f3
RaizaArce <raiza.nataly.arce@gmail.com>
2026-05-21 01:02:00 -0500

    feat: agrego navbar con permisos por rol, mensajes flash y catalogo de platos

--- menu/urls.py
+++ menu/urls.py
@@ -5,7 +5,7 @@ from . import views
 
 urlpatterns = [
     # Catalogo de platos organizado por categoria
-    # path('menu/', views.catalogo_platos, name='catalogo_platos'),
+    path('menu/', views.catalogo_platos, name='catalogo_platos'),
 
     # Gestion de menu (CRUD de platos y categorias)
     # path('menu/gestion/', views.gestion_menu, name='gestion_menu'),
