4525e4f57a9c3529d0f580de4ed4ec496906d9fd
RaizaArce <raiza.nataly.arce@gmail.com>
2026-05-21 01:02:00 -0500

    feat: agrego navbar con permisos por rol, mensajes flash y catalogo de platos

--- menu/views.py
+++ menu/views.py
@@ -1,3 +1,13 @@
 
-
+#Catálogo de Platos
+#Archivo: menu/views.py
+#parte del commit: feat: agrego navbar con permisos por rol, mensajes flash y catalogo de platos
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from menu.models import Categoria
+
+@login_required
+def catalogo_platos(request):
+    categorias = Categoria.objects.prefetch_related('platos').all()
+    return render(request, 'menu/catalogo_platos.html', 
+                {'categorias': categorias})
