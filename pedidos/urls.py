78b8f60c26b2b2e8578ada1a1e78889346c7a2f3
edwargreoz <gonzalzedwar@gmail.com>
2026-05-21 14:51:57 -0500

    feat:  Se realizo los views de pedidos incluyendo el KDS DE COCINA. incluyendo las urls

--- pedidos/urls.py
+++ pedidos/urls.py
@@ -4,6 +4,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # Tomar pedido: catalogo de platos + comanda activa
-    # path('pedidos/tomar/<int:mesa_id>/', views.tomar_pedido, name='tomar_pedido'),
+    # Tomar Pedido
+    path('pedidos/tomar/<int:mesa_id>/', views.tomar_pedido, name='tomar_pedido'),
+    path('pedidos/agregar/<int:comanda_id>/', views.agregar_platos_pedido, name='agregar_platos_pedido'),
+    # KDS Cocina
+    path('cocina/', views.kds_panel, name='kds_panel'),
+    path('cocina/enviar/<int:linea_id>/', views.enviar_cocina, name='enviar_cocina'),
+    path('cocina/listo/<int:linea_id>/', views.marcar_listo, name='marcar_listo'),
 ]
