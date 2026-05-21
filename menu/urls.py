e93df2870443de559451ec9a5a37567898310080
edwargreoz <gonzalzedwar@gmail.com>
2026-05-21 02:31:06 -0500

    feat: views y url de menu ya implementados

--- menu/urls.py
+++ menu/urls.py
@@ -6,7 +6,10 @@ from . import views
 urlpatterns = [
     # Catalogo de platos organizado por categoria
     path('menu/', views.catalogo_platos, name='catalogo_platos'),
-
-    # Gestion de menu (CRUD de platos y categorias)
-    # path('menu/gestion/', views.gestion_menu, name='gestion_menu'),
+    path('menu/gestion/', views.gestion_menu, name='gestion_menu'),
+    path('menu/crear-categoria/', views.crear_categoria, name='crear_categoria'),
+    path('menu/crear-plato/', views.crear_plato, name='crear_plato'),
+    path('menu/editar/<int:plato_id>/', views.editar_plato, name='editar_plato'),
+    path('menu/eliminar/<int:plato_id>/', views.eliminar_plato, name='eliminar_plato'),
+    
 ]
