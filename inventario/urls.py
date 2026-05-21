cedf223b84a2e62d047160271745554fb99a53e4
edwargreoz <gonzalzedwar@gmail.com>
2026-05-21 15:09:06 -0500

    feat: views de inventario listos y urls creadas

--- inventario/urls.py
+++ inventario/urls.py
@@ -4,6 +4,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # Lista de insumos con stock y alertas
-    # path('inventario/', views.lista_insumos, name='lista_insumos'),
+    path('inventario/', views.lista_insumos, name='lista_insumos'),
+    path('inventario/gestion/', views.gestion_insumos, name='gestion_insumos'),
+    path('inventario/crear/', views.crear_insumo, name='crear_insumo'),
+    path('inventario/editar/<int:insumo_id>/', views.editar_insumo, name='editar_insumo'),
+    path('inventario/eliminar/<int:insumo_id>/', views.eliminar_insumo, name='eliminar_insumo'),
 ]
