d621a1addaf207abf3c0bf379b8542a7765a3a2a
edwargreoz <gonzalzedwar@gmail.com>
2026-05-21 23:41:06 -0500

    fix: Corrección completa del sistema de stock: ahora se audita cada movimiento, se restaura el inventario al anular una comanda y se evitó que el stock quede negativo

--- inventario/admin.py
+++ inventario/admin.py
@@ -1,6 +1,7 @@
 
 from django.contrib import admin
-from .models import Insumo, RecetaInsumo
+from .models import Insumo, RecetaInsumo, MovimientoInsumo
 
 admin.site.register(Insumo)
 admin.site.register(RecetaInsumo)
+admin.site.register(MovimientoInsumo)
