588c0d9ac56096c52d5c62074a6f8e9954f4eabd
RaizaArce <raiza.nataly.arce@gmail.com>
2026-07-06 12:45:22 -0500

    feat: crear excepciones de dominio para services

---
+++ core/excepciones.py
@@ -0,0 +1,30 @@
+
+class RecursoNoEncontrado(Exception):
+    pass
+
+class StockInsuficiente(Exception):
+    pass
+
+class ProductoSinStock(Exception):
+    pass
+
+class UnidadConversionInvalida(Exception):
+    pass
+
+class ReglaNegocioViolada(Exception):
+    pass
+
+class CajaNoAbierta(Exception):
+    pass
+
+class MontoInvalido(Exception):
+    pass
+
+class ReferenciaInvalida(Exception):
+    pass
+
+class MesaConComandaActiva(Exception):
+    pass
+
+class UnionInvalida(Exception):
+    pass
