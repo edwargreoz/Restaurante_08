4f75bbdf08de5ab08a0c9cde0bd281de1f55d649
edwargreoz <gonzalzedwar@gmail.com>
2026-07-08 22:42:55 -0500

    fix: contenido de routing.py y consumers

---
+++ config/routing.py
@@ -0,0 +1,16 @@
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
+from django.urls import re_path
+from consumers import kds_consumer, plano_consumer, comanda_consumer
+
+websocket_urlpatterns = [
+    re_path(r'ws/kds/$', kds_consumer.KDSConsumer.as_asgi()),
+    re_path(r'ws/plano/$', plano_consumer.PlanoConsumer.as_asgi()),
+    re_path(r'ws/comanda/(?P<comanda_id>\d+)/$', comanda_consumer.ComandaConsumer.as_asgi()),
+]
+
+application = ProtocolTypeRouter({
+    'websocket': AuthMiddlewareStack(
+        URLRouter(websocket_urlpatterns)
+    ),
+})
