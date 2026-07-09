c6a9ecda24028e3a6e45752f2a078431829442f6
Metroxtt <cbrionesmarkang@uss.edu.pe>
2026-07-08 23:50:38 -0500

    fix: agregar http handler a ProtocolTypeRouter en routing.py

--- config/routing.py
+++ config/routing.py
@@ -1,15 +1,18 @@
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 from django.urls import re_path
+from django.core.asgi import get_asgi_application
 from consumers import kds_consumer, plano_consumer, comanda_consumer
 
 websocket_urlpatterns = [
     re_path(r'ws/kds/$', kds_consumer.KDSConsumer.as_asgi()),
     re_path(r'ws/plano/$', plano_consumer.PlanoConsumer.as_asgi()),
+    re_path(r'ws/kds/$', kds_consumer.KDSConsumer.as_asgi()),
     re_path(r'ws/comanda/(?P<comanda_id>\d+)/$', comanda_consumer.ComandaConsumer.as_asgi()),
 ]
 
 application = ProtocolTypeRouter({
+    'http': get_asgi_application(),
     'websocket': AuthMiddlewareStack(
         URLRouter(websocket_urlpatterns)
     ),
