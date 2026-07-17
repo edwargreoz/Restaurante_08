5f67bd4744b05b9f02f84ec453c4a3d58f4b205c
linnexnami <leonardo.mdina2003@gmail.com>
2026-07-17 02:06:46 -0500

    Fix: Implementación del método listar en CajaRepository

--- infraestructura/persistencia/repositorios/caja_repo.py
+++ infraestructura/persistencia/repositorios/caja_repo.py
@@ -1,5 +1,5 @@
 
-from typing import Optional
+from typing import Optional, List
 from caja.models import Caja as CajaModel
 from dominio.entidades.caja import Caja
 
@@ -20,3 +20,10 @@ class CajaRepository:
             }
         )
         return caja
+
+    def listar(self) -> List[Caja]:
+        return [
+            Caja(id=c.id, turno=c.turno, cajero_id=c.cajero_id,
+                 saldo_inicial=c.saldo_inicial, estado=c.estado)
+            for c in CajaModel.objects.all()
+        ]
